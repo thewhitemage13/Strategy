7370ced3202abdc5c4f481ca98e0f32033040aa7
Mukhammed Lolo <144810207+thewhitemage13@users.noreply.github.com>
2024-05-25 17:07:19 +0200

    Add files via upload

---
+++ Strategy - C++/Strategy - C++/Strategy - C++.cpp
@@ -0,0 +1,59 @@
+﻿#include <iostream>
+using namespace std;
+
+class Reader {
+public:
+    virtual ~Reader(){}
+    virtual void Parse(string& url) = 0;
+};
+
+class ResourceReader {
+private:
+    Reader* reader;
+public:
+    ResourceReader(Reader* r) : reader(r) {};
+    ~ResourceReader() { delete reader; }
+    
+    void SetStrategy(Reader* r) {
+        if (reader != nullptr)delete reader;
+        reader = r;
+    }
+    void Read(string& url) {
+        reader->Parse(url);
+    }
+};
+
+class NewsSiteReader : public Reader {
+public:
+    void Parse(string& url) override {
+        cout << "news site parsing: " << url << "\n";
+    }
+};
+
+class SocialNetworkReader : public Reader {
+public:
+    void Parse(string& url) override {
+        cout << "parsing news feed from a social network: " << url << "\n";
+    }
+};
+
+class TelegramChannelReader : public Reader {
+public:
+    void Parse(string& url) override {
+        cout << "Telegram messenger channel parsing: " << url << "\n";
+    }
+};
+
+int main() {
+    ResourceReader* res = new ResourceReader(new NewsSiteReader());
+    string url = "https://news.com";
+    res->Read(url);
+
+    url = "https://facebook.com";
+    res->SetStrategy(new SocialNetworkReader());
+    res->Read(url);
+
+    url = "@news_channel_telegram";
+    res->SetStrategy(new TelegramChannelReader());
+    res->Read(url);
+}
